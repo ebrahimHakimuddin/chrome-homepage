674694477cb9ed821c1cbd5eba1e1ee6110411f3
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2025-02-26 00:35:46 +0300

    Made it Actually get Top Sites

--- src/components/top-sites/top-sites.tsx
+++ src/components/top-sites/top-sites.tsx
@@ -4,73 +4,72 @@ type TopSite = {
   title: string;
 };
 export default function TopSites() {
-  //   const [topSites, setTopSites] = useState<TopSite[]>();
+  const [topSites, setTopSites] = useState<TopSite[]>();
   const [loading, setLoading] = useState(false);
-  // //   useEffect(() => {
-  //     const fetchTopSites = async () => {
-  //       try {
-  //         const sites = await chrome.topSites.get();
-  //         console.log(sites);
-  //         setTopSites(sites);
-  //         setLoading(false);
-  //       } catch (err) {
-  //         console.error("Error fetching top sites:", err);
-  //         // setError(err instanceof Error ? err.message : "Unknown error");
-  //         setLoading(false);
-  //       }
-  //     };
+  useEffect(() => {
+    const fetchTopSites = async () => {
+      try {
+        const sites = await chrome.topSites.get();
+        console.log(sites);
+        setTopSites(sites);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching top sites:", err);
+        // setError(err instanceof Error ? err.message : "Unknown error");
+        setLoading(false);
+      }
+    };
 
-  //     fetchTopSites();
-  //     // console.log(topSites);
-  //   }, []);
-  const tempsites = [
-    {
-      title: "YouTube",
-      url: "https://www.youtube.com/?feature=ytca",
-    },
-    {
-      title: "New Tab",
-      url: "http://localhost:5173/",
-    },
-    {
-      title: "T3 Chat - The Fastest AI Chatbot",
-      url: "https://t3.chat/",
-    },
-    {
-      title: "HG8245H5",
-      url: "http://192.168.100.1/",
-    },
-    {
-      title: "Prime Video | Watch movies, TV shows, Live TV, and sports",
-      url: "https://www.primevideo.com/",
-    },
-    {
-      title: "Login",
-      url: "https://aris3.udsm.ac.tz/",
-    },
-    {
-      title: "Download Client » Free Linux PC Games",
-      url: "https://freelinuxpcgames.com/download-client/",
-    },
-    {
-      title: "Home - Netflix",
-      url: "https://www.netflix.com/",
-    },
-    {
-      title: "The Official Home of YIFY Movies Torrent Download - YTS",
-      url: "https://yts.mx/",
-    },
-    {
-      title: "GitHub",
-      url: "https://github.com/",
-    },
-  ];
+    fetchTopSites();
+    // console.log(topSites);
+  }, []);
+  //     {
+  //       title: "YouTube",
+  //       url: "https://www.youtube.com/?feature=ytca",
+  //     },
+  //     {
+  //       title: "New Tab",
+  //       url: "http://localhost:5173/",
+  //     },
+  //     {
+  //       title: "T3 Chat - The Fastest AI Chatbot",
+  //       url: "https://t3.chat/",
+  //     },
+  //     {
+  //       title: "HG8245H5",
+  //       url: "http://192.168.100.1/",
+  //     },
+  //     {
+  //       title: "Prime Video | Watch movies, TV shows, Live TV, and sports",
+  //       url: "https://www.primevideo.com/",
+  //     },
+  //     {
+  //       title: "Login",
+  //       url: "https://aris3.udsm.ac.tz/",
+  //     },
+  //     {
+  //       title: "Download Client » Free Linux PC Games",
+  //       url: "https://freelinuxpcgames.com/download-client/",
+  //     },
+  //     {
+  //       title: "Home - Netflix",
+  //       url: "https://www.netflix.com/",
+  //     },
+  //     {
+  //       title: "The Official Home of YIFY Movies Torrent Download - YTS",
+  //       url: "https://yts.mx/",
+  //     },
+  //     {
+  //       title: "GitHub",
+  //       url: "https://github.com/",
+  //     },
+  //   ];
   return (
     <div className="rounded-xl border  border-gray-900 p-5 bg-[#191919] mt-10">
       <h1 className="text-xl font-bold text-center">Top Sites:</h1>
       {loading ? null : (
         <div className="grid grid-cols-3 justify-center">
-          {tempsites!.map((item, index) => {
+          {topSites!.map((item, index) => {
             let title = "";
             if (index <= 8) {
               if (item.title.includes("-")) {
